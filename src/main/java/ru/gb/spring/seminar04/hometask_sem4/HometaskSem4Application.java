60319a46a3b2c1890fd438c4cb487eb23e012b1f
Volkov Aleksandr <222volkov@gmail.com>
2024-05-11 06:27:20 +0300

    Initial commit Spring Boot with Thymeleaf, Web dependencies

---
+++ src/main/java/ru/gb/spring/seminar04/hometask_sem4/HometaskSem4Application.java
@@ -0,0 +1,13 @@
+package ru.gb.spring.seminar04.hometask_sem4;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HometaskSem4Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HometaskSem4Application.class, args);
+	}
+
+}
---
+++ src/test/java/ru/gb/spring/seminar04/hometask_sem4/HometaskSem4ApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.gb.spring.seminar04.hometask_sem4;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HometaskSem4ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
